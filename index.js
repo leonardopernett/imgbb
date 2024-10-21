78d98170b00e7bf410848ee607402a7183aed04a
leonardo pernett <lnard.dev@gmail.com>
2024-10-21 17:31:30 -0500

    second commit

---
+++ index.js
@@ -0,0 +1,43 @@
+import express from 'express'
+import { extname, resolve } from 'path'
+import multer from 'multer'
+import imgBB from 'imgbb-uploader'
+import fs from 'fs-extra'
+import atob from 'atob'
+
+const TOKEN = '8660860ec379863415d98e882c7f0bce'
+
+const app = express()
+
+const storage = multer.diskStorage({
+  destination: resolve('./public/uploads'),
+  filename: (req, file, cb)=>{
+    cb(null,Date.now() +extname(file.originalname))
+  }
+})
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(multer({ storage }).single('image'))
+
+app.post('/uploads', async (req,res) => {
+  const token  = req.headers.authorization.split(' ')[1]
+  const valid = 'Konecta2024**'
+
+  if( valid == atob(token).toString()){
+    await fs.unlink( resolve('./public/uploads/'+req.file.filename))
+    return res.status(400).json('invalid credentials')
+  }
+
+  try {
+   const response = await imgBB(`https://api.imgbb.com/1/upload?expiration=600&key=${TOKEN}`, resolve('./public/uploads/'+req.file.filename))
+    await fs.unlink( resolve('./public/uploads/'+req.file.filename))
+    res.json(response) 
+  } catch (error) {
+    res.json(error.message)
+  }
+})
+
+app.use(express.static(resolve('./public')))
+
+app.listen(3000, () => console.log('server on port 3000'))
